2eee0498695a3cdca7b9ad086f91cdd195f24ff2
ArmaanKafaipour <k_armaan@yahoo.com>
2022-02-18 01:20:22 -0800

    testing code pipeline and lambda function for invalidation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,9 +18,9 @@ export default function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="left">
-        {/* <div className="imgContainer">
+        <div className="imgContainer">
           <img src="assets/portraitPlaceholder.png" alt="" />
-        </div> */}
+        </div>
       </div>
       <div className="right">
         <div className="wrapper">
